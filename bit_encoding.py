ebab84578c99aed5102d3aa8117c181854850b01
thsieh10 <thsieh10@gmail.com>
2017-05-11 09:51:08 -0400

    building decision tree with bit encodings

--- bit_encoding.py
+++ bit_encoding.py
@@ -3,6 +3,23 @@ import argparse
 from pprint import pprint
 
 
+def get_mutual_information(i, j, clusters):
+	return "hi"
+
+def read_bit_vectors(encodings, text):
+	bit_vectors = []
+	quadgram = []
+
+	for ch in text:
+		quadgram.append(ch)
+		if len(quadgram) == 4:
+			bit_vector = ""
+			for i in range(3):
+				bit_vector += encodings[quadgram[i]]
+			bit_vectors.append((bit_vector, ch))
+			quadgram.pop(0)
+	return bit_vectors
+
 def bit_encoding(encodings, train_text):
 	# split into dev and heldout data
 	train = open(train_text, 'r')
@@ -10,20 +27,20 @@ def bit_encoding(encodings, train_text):
 	dev = text[0:int(len(text) * 0.8)]
 	held = text[int(-len(text) * 0.2):]
 
-	quadgram = []
-	for c in dev:
-		if len(quadgram) == 4:
-			quadgram.pop(0)
+	dev_bit_vectors = read_bit_vectors(encodings, dev)
+	held_bit_vectors = read_bit_vectors(encodings, held)
 
-		quadgram.append(c)
+	pos = [9 * i for i in range(3)]
+	# maximize mutual information
 
-	return "hi"
+	print dev_bit_vectors
+
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("train_text", help="file name of training text")
 	args = parser.parse_args()
 	train_text = args.train_text
-	encodings = ""
-	#encodings = cluster.main()
+	encodings = cluster.main()
 	bit_encoding(encodings, train_text)
