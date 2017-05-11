240afa56b53680c98164e9a450ff2ad1b866dcba
thsieh10 <thsieh10@gmail.com>
2017-05-11 08:09:09 -0400

    readings quadgrams

--- bit_encoding.py
+++ bit_encoding.py
@@ -2,7 +2,20 @@ import cluster
 import argparse
 from pprint import pprint
 
-def bit_encoding(encodings):
+
+def bit_encoding(encodings, train_text):
+	# split into dev and heldout data
+	train = open(train_text, 'r')
+	text = train.read()
+	dev = text[0:int(len(text) * 0.8)]
+	held = text[int(-len(text) * 0.2):]
+
+	quadgram = []
+	for c in dev:
+		if len(quadgram) == 4:
+			quadgram.pop(0)
+
+		quadgram.append(c)
 
 	return "hi"
 
@@ -11,6 +24,6 @@ if __name__ == '__main__':
 	parser.add_argument("train_text", help="file name of training text")
 	args = parser.parse_args()
 	train_text = args.train_text
-
-	encodings = cluster.main()
-	bit_encoding(encodings)
+	encodings = ""
+	#encodings = cluster.main()
+	bit_encoding(encodings, train_text)
