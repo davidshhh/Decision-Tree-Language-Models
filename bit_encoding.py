b45671fc0f58c7b8fa6fcff2e01422de9b8b5fe6
thsieh10 <thsieh10@gmail.com>
2017-05-12 02:40:32 -0400

    cross validation for heldout, checks for threshold

--- bit_encoding.py
+++ bit_encoding.py
@@ -6,19 +6,68 @@ from math import log
 from copy import deepcopy
 import sys
 
-def get_history_cond_prob(h1, h2, history, dev_bit_vectors):
-	return 0
+def compute_entropy(node, bigram, phi, con_prob_phi):
+	entropy = 0.0
+	prev = (0,)
+
+	visited = set()
+	for (w, bv) in node:
+		if len(prev) > 1 and (prev, (w, bv)) not in visited:
+			visited.add((prev, (w, bv)))
+			entropy -= bigram[(prev, (w, bv))] * log(con_prob_phi[phi[prev]][(w, bv)], 2)
+		prev = (w, bv)
+	return entropy
+
+def build_con_prob(node, phi):
+	prev = (0,)
+	con_prob_phi = defaultdict(lambda: defaultdict(int))
+	for (w, bv) in node:
+		if len(prev) > 1:
+			con_prob_phi[phi[prev]][(w, bv)] += 1
+		prev = (w, bv)
+	con_prob_phi = {phi_w1 : {w2 : (float(count) / sum(w2_counts.values())) for w2, count in w2_counts.iteritems()} for phi_w1, w2_counts in con_prob_phi.iteritems()}
+	return con_prob_phi
+
+def build_phi(history):
+	phi = {}
+	for i, (h, _) in enumerate(history):
+		for (ch, bv) in h:
+			phi[(ch, bv)] = i
+	# SMOOTH
+	return defaultdict(int, phi)
+
+def get_entropy_text(text, history):
+	phi = build_phi(history)
+	con_prob = build_con_prob(text, phi)
+	bigram = get_bigram(text)
+	entropy = compute_entropy(text, bigram, phi, con_prob)
+
+	return entropy
+
+def get_bigram(text):
+	bigram = defaultdict(int)
+	prev = (0,)
+
+	# collect bigram statistics
+	for (ch, bv) in text:
+
+		if len(prev) > 1:
+			bigram[(prev, (ch, bv))] += 1
+		prev = (ch, bv)
+	
+	# normalize bigram of vectors to frequencies for p(w1, w2) calculation
+	total_bigram = sum(bigram.values())
+	bigram = defaultdict(float, {k : float(v) / total_bigram for k, v in bigram.iteritems()})
+	
+	return bigram
 
 
-def get_mutual_information(question, node):
+def get_entropy(question, node):
 	# new histories
 	(phi1, w1) = ([], 0)
 	(phi2, w2) = ([], 0)
 
-	# collect bigram statistics for the text at this node
 	prev = (0,)
-	bigram = defaultdict(int)
-
 	# split data by question of frontier
 	for (ch, bv) in node:
 		if bv[question] ==  "0":
@@ -27,13 +76,11 @@ def get_mutual_information(question, node):
 		else:
 			phi2.append((ch, bv))
 			w2 += 1
-		if len(prev) > 1:
-			bigram[(prev, (ch, bv))] += 1
+
 		prev = (ch, bv)
 
-	# normalize bigram of vectors to frequencies for p(w1, w2) calculation
-	total_bigram = sum(bigram.values())
-	bigram = defaultdict(float, {k : float(v) / total_bigram for k, v in bigram.iteritems()})
+	# count bigrams for p(w1, w2)
+	bigram = get_bigram(node)
 
 	# normalize weights of each split
 	w1 = float(w1) / len(node)
@@ -42,41 +89,22 @@ def get_mutual_information(question, node):
 	split = [(phi1, w1), (phi2, w2)]
 
 	# setup look up tables for phi prob amd conditional phi prob
-	phi = {}
-	for i, (h, w) in enumerate(split):
-		for (ch, bv) in h:
-			phi[(ch, bv)] = i
+	#phi = build_phi(split)
 
-	prob_phi = {i : w for i, (_, w) in enumerate(split)}
-	con_prob_phi = defaultdict(lambda : defaultdict(int))
+	phi = build_phi(split)
+
+	con_prob_phi = build_con_prob(node, phi)
 
 	# compute the entropy of the predicted letter for each permissible question
 	# based on the development data that reaches that node
-	prev = (0,)
-	for (w, bv) in node:
-		if len(prev) > 1:
-			con_prob_phi[phi[prev]][(w, bv)] += 1
-		prev = (w, bv)
 
-	con_prob_phi = {phi_w1 : {w2 : (float(count) / sum(w2_counts.values())) for w2, count in w2_counts.iteritems()} for phi_w1, w2_counts in con_prob_phi.iteritems()}
+	entropy = compute_entropy(node, bigram, phi, con_prob_phi)
 
-	entropy = 0.0
-	prev = (0,)
-
-	visited = set()
-	for (w, bv) in node:
-		if len(prev) > 1 and (prev, (w, bv)) not in visited:
-			visited.add((prev, (w, bv)))
-			entropy += bigram[(prev, (w, bv))] * log(con_prob_phi[phi[prev]][(w, bv)], 2)
-		prev = (w, bv)
-
-	print entropy
 	return (entropy, split)
 
 def read_bit_vectors(encodings, text):
 	bit_vectors = []
 	quadgram = []
-	bigram = defaultdict(int)
 
 	# load (l1, l2, l3) into bit vectors
 	prev = ""
@@ -92,7 +120,7 @@ def read_bit_vectors(encodings, text):
 
 			quadgram.pop(0)
 
-	return (bigram, bit_vectors)
+	return bit_vectors
 
 def bit_encoding(encodings, train_text, f_bigram):
 	# split into dev and heldout data
@@ -101,8 +129,8 @@ def bit_encoding(encodings, train_text, f_bigram):
 	dev = text[0:int(len(text) * 0.8)]
 	held = text[int(-len(text) * 0.2):]
 
-	(dev_bigram, dev_bit_vectors) = read_bit_vectors(encodings, dev)
-	(held_bigram, held_bit_vectors) = read_bit_vectors(encodings, held)
+	dev_bit_vectors = read_bit_vectors(encodings, dev)
+	held_bit_vectors = read_bit_vectors(encodings, held)
 
 	# starting positions of each question
 	questions = [9 * i for i in range(3)]
@@ -118,18 +146,32 @@ def bit_encoding(encodings, train_text, f_bigram):
 
 		(node, qs) = frontiers.pop(0)
 		# maximize mutual information asked by question and choose the question as 'candidate'
-		(q_can, mt_can, split_can) = (-1, -1, [])
+		(q_can, en_can, split_can) = (-1, -1, [])
 
 		# for each of the three questions, check mutual information and find 'candidate'
 		for q in range(len(qs)):
-			(mt, split) = get_mutual_information(qs[q], node)
-			if mt > mt_can:
-				(q_can, mt_can, split_can) = (q, mt, split)
+			(en, split) = get_entropy(qs[q], node)
+			if en > en_can:
+				(q_can, en_can, split_can) = (q, en, split)
 
 		# check the entropy reduction on the held out data by splitting this candidate
+		(entropy, _) = get_entropy(q_can, held_bit_vectors)
+		entropy_ori = get_entropy_text(held_bit_vectors, history)
 
+		entropy_reduction = entropy_ori - entropy
+		print entropy
+		print entropy_ori
+		print entropy_reduction
 
 		# split node
+		if entropy_reduction > 0.005:
+
+		 	# locate frontier in history and create new history
+		 #	for i, (h, w) in enumerate(history):
+		 #		if node == h:
+		 #			del new_history[i]
+	 
+	 	#	history = history + [(phi1, w1), (phi2, w2)]
 
 
 		# declare node terminal
